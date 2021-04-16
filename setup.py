f491f4be7036766111c893b9e6fcfdf962105b69
cofob <cofob@tutanota.com>
2021-04-16 21:07:08 +0700

    Created setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+def add(name: str, description: str = ''):
+    with open('env.sh', 'a+') as file:
+        file.write(f'export {name}='+input(f'{name}:\n\t' +
+                                        (f'description: {description}\n\t' if description else '')
+                                        + 'value: ') + '\n')
+
+
+f = open('env.sh', 'w')
+f.close()
+
+add('API_ID', 'api_id from my.telegram.org app page')
+add('API_HASH', 'api_hash from my.telegram.org app page')
+add('OWNER_ID', 'bot owner telegram id, from @userinfobot')
