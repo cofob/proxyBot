167fee23b41e0e9c084c152aac64d43486c4c24d
cofob <cofob@tutanota.com>
2021-04-18 00:10:03 +0700

    Added new fields in setup

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 import os
+import pickle
+
+try:
+    os.remove('database.db')
+    os.remove('env.sh')
+except:
+    pass
+
+import models
 
 os.system('msgfmt locale/ru/LC_MESSAGES/bot.po -o locale/ru/LC_MESSAGES/bot.mo')
 
@@ -10,10 +19,17 @@ def add(name: str, description: str = ''):
                                         + 'value: ') + '\n')
 
 
-f = open('env.sh', 'w')
-f.close()
+def add_to_db(name: str, description: str = ''):
+    value = input(f'{name}:\n\t' + (f'description: {description}\n\t' if description else '') + 'value: ')
+    m = models.Setting()
+    m.name = name
+    m.value = pickle.dumps(value)
+    m.save()
 
-add('API_ID', 'api_id from my.telegram.org app page')
-add('API_HASH', 'api_hash from my.telegram.org app page')
+
+add_to_db('qiwi_token', 'token from https://qiwi.com/api')
+add_to_db('qiwi_phone', 'phone number from qiwi')
+add('API_ID', 'api_id from https://my.telegram.org app page')
+add('API_HASH', 'api_hash from https://my.telegram.org app page')
 add('OWNER_ID', 'bot owner telegram id, from @userinfobot')
 add('BOT_TOKEN', 'bot token from @botfather')
